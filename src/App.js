d5bc29d21489be5a32dd0a4cd2bf044b578bc124
starbuckshiker <u1304954@umail.utah.edu>
2020-11-25 15:07:30 -0700

    Todo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
+import Todos from './components/Todos';
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'New Light-weight Road Bike',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Remote start for Ram',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Chest mount HR monitor',
+        completed: false
+      }
+    ]
+
+    }
+  
+
   render() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos}/>
+      </div>
   );
 }
 }
