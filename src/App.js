b16db5081ca12a19a4a921cee824e4dc739031c3
starbuckshiker <u1304954@umail.utah.edu>
2020-11-25 17:05:46 -0700

    add item

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+
 import './App.css';
 
 class App extends Component {
@@ -42,9 +44,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <div className="container">
+          <Header />
+        <AddTodo />
         <Todos todos={this.state.todos} markComplete ={this.markComplete}
         delTodo= {this.delTodo}/>
+        </div>
       </div>
   );
 }
