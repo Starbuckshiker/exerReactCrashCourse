ab663af113b56def2f895ff909250a243381ef4e
starbuckshiker <u1304954@umail.utah.edu>
2020-11-25 17:13:44 -0700

    add to

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 
 export class AddTodo extends Component {
+state = {
+    title: ''
+}
+
+onChange = (e) => this.setState({ title: e.target.vaule });
+
     render() {
         return (
            <form style={{ display: 'flex'}}>
@@ -9,7 +15,8 @@ export class AddTodo extends Component {
                  name="title" 
                  style={{ flex:'10', padding: '5px'}}
                  placeholder= "Add to Christmas List ..." 
-
+                value={this.state.title}
+                onChange={this.onChange}
                 />
                 <input type="submit"
                 value="Submit"
